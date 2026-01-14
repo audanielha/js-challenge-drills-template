b9570c06367d58dea0f22a714c5978163fcf6dc3
audanielha <audanielha@gmail.com>
2026-01-14 20:48:00 +1300

    Replace script.js with simple-shopping-cart.js and implement shopping cart functionality

---
+++ js/simple-shopping-cart.js
@@ -0,0 +1,28 @@
+
+
+let shoppingCart = [];
+
+let addItem = (itemName)=>{
+    shoppingCart.push(itemName);
+    console.log(`Added ${itemName} to cart.`)
+}
+
+addItem("Apple");
+addItem("Orange");
+addItem("Pear");
+
+let removeItem = (itemName) =>{
+   let itemIndex = shoppingCart.indexOf(itemName);
+   shoppingCart.splice(itemIndex, 1);
+   console.log(`Removed ${itemName} from cart.`)
+}
+
+
+removeItem("Pear");
+
+let currentItems = (items) =>{
+    // shoppingCart.length === 0 ? items = "No items in cart." : items = shoppingCart.join(", ");
+    shoppingCart.length === 0 ? items = "No items in card." : items = shoppingCart.join(", ");
+    console.log(`Current items in cart: ${items}`);
+}
+currentItems();
