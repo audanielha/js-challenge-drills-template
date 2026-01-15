440a6f741efc7bb286964f871a791479e61fc8ad
audanielha <audanielha@gmail.com>
2026-01-15 23:29:14 +1300

    sentence and word manipulation challenge

---
+++ js/sentence-and-word-manipulation.js
@@ -0,0 +1,16 @@
+
+function manipulateSentence(sentence, word) {
+    // Your implementation here
+    console.log(sentence, word)
+
+}
+// Sample Input
+const inputSentence = "JavaScript is fun";
+const inputWord = "Awesome";
+// Expected Output
+// {
+//   newSentence: "Awesome JavaScript is fun",
+//   modifiedArray: ["AwesomeJavaScript", "Awesomeis", "Awesomefun"]
+// }
+const result = manipulateSentence(inputSentence, inputWord);
+console.log(result);
